8977741faada00de872ba56766f47414fd8f3c54
Andrew Sardone <andrew@andrewsardone.com>
2017-01-02 22:49:35 -0500

    Add intentional analysis issue to B.php

--- src/B.php
+++ src/B.php
@@ -4,6 +4,11 @@ namespace My\Example;
 
 class B {
     function doStuffWithClassA() {
+        // B.php depends on A.php here
         echo (new A())->getStatuscode();
+
+        // Introduce an intentional issue to be detected by etsy/phan analysis.
+        $foo = 1;
+        $foo->length;
     }
 }
